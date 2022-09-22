9d8d3714fc65c42824e36ff2699f0a5647953fea
jack m <31999176+melchj@users.noreply.github.com>
2022-09-21 22:41:10 -0400

    Adds basic background images

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { useState } from 'react'
+import bgImg01 from "./assets/pexels-ahmed-13326533.jpg"
+import bgImg02 from "./assets/pexels-aron-visuals-3596370.jpg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <p className='text-center text-green-500 bold'>
-      hello world
-    </p>
+    <div className='container'>
+      <p className='text-center text-green-500 bold'>
+        hello world
+      </p>
+      <div className='relative'>
+        <div className='relative'>
+          <div className='h-screen'
+          style={{
+            backgroundImage: `url(${bgImg01})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center"
+            }}>
+          </div>
+          <div className='h-screen'
+          style={{
+            backgroundImage: `url(${bgImg02})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center"
+            }}>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
