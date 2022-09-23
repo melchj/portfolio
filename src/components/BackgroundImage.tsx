86b9f8c01fe443a8a10d256c57855e4fc38b7b64
jack m <31999176+melchj@users.noreply.github.com>
2022-09-23 08:10:23 -0400

    add parallax to background images

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -1,13 +1,16 @@
 
+// TODO: need to look into turning off parallax scrolling for tablets/phones
+// https://www.w3schools.com/howto/howto_css_parallax.asp
 function BackgroundImage(props: {title: string, img: string}) {
     return (
         <div className='h-screen flex justify-center items-center'
         style={{
             backgroundImage: `url(${props.img})`,
             backgroundSize: "cover",
-            backgroundPosition: "center center"
+            backgroundPosition: "center center",
+            backgroundAttachment: "fixed"
             }}>
-            <h2 className="text-white">{props.title}</h2>
+            <h2 className="text-white text-4xl font-bold">{props.title}</h2>
         </div>
     )
 }
